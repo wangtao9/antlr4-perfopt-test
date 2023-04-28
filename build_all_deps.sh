0f885417a7412e98e148c9fccc8b09d314597269
wangtao9 <wangtaofighting@163.com>
2023-04-28 05:50:03 +0000

    build all deps

---
+++ build_all_deps.sh
@@ -0,0 +1,23 @@
+
+# build wangtao9/antlr4
+TOP_LEVEL_DIR=${PWD}
+git clone https://github.com/wangtao9/antlr4.git
+cd antlr4 && git checkout -b feat_lock_free_cpp_target origin/feat_lock_free_cpp_target && mvn -DskipTests install
+
+
+# generate cpp lexer & parser
+cd $TOP_LEVEL_DIR
+ANTLR4_JAR=${PWD}/antlr4/tool/target/antlr4-4.12.1-SNAPSHOT-complete.jar
+java -jar ${ANTLR4_JAR} -Dlanguage=Cpp -visitor -o src/generated -package parser Cypher.g4
+
+
+# build antlr4-cpp-runtime
+# see https://github.com/antlr/antlr4/tree/master/runtime/Cpp
+cd antlr4/runtime/Cpp
+mkdir build && mkdir run && cd build
+cmake .. -DCMAKE_BUILD_TYPE=Release -DWITH_DEMO=0
+make -j8
+DESTDIR=../run make install
+cd $TOP_LEVEL_DIR
+
+
---
+++ src/main.cpp
@@ -0,0 +1,61 @@
+// created by wangtao9
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <unistd.h>
+#include "antlr4-runtime.h"
+#include "generated/CypherLexer.h"
+#include "generated/CypherParser.h"
+#include "generated/CypherBaseVisitor.h"
+
+using namespace antlr4;
+using namespace parser;
+
+const static int QUERY_COUNT = 10000;
+
+const static std::string query_0 = "CALL foo()";
+const static std::string query_1 = "RETURN 1";
+const static std::string query_2 =
+    "MATCH (n:Person {id:2000})-[:knows]->(friend)-[:located_in]->(city) "
+    "RETURN friend, count(city) ORDER BY friend.id LIMIT 100";
+
+void proc_pure_parse(int &arg) {
+  auto t0 = std::chrono::high_resolution_clock::now();
+  for (int i = 0; i < QUERY_COUNT; i++) {
+      ANTLRInputStream input(query_2);
+      CypherLexer lexer(&input);
+      CommonTokenStream tokens(&lexer);
+      CypherParser parser(&tokens);
+      tree::ParseTree *tree = parser.oC_Cypher();
+      CypherBaseVisitor visitor;
+      tree->accept(&visitor);
+  }
+  auto t1 = std::chrono::high_resolution_clock::now();
+  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0);
+  std::cout << duration.count() << "ms\n";
+}
+
+
+void parser_pure_parse_test(int num_thread) {
+  std::cout << "[main thread]: " << __func__ << std::endl;
+  std::vector<std::thread> threads;
+  auto t0 = std::chrono::high_resolution_clock::now();
+  for (int i = 0; i < num_thread; i++) {
+    threads.emplace_back(proc_pure_parse, std::ref(i));
+  }
+  // threads join
+  for (auto &t : threads) t.join();
+  auto t1 = std::chrono::high_resolution_clock::now();
+  auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(t1 - t0);
+  std::cout << "[main thread]: " << duration.count() << "ms, "
+   << "throughput: " << (double)(QUERY_COUNT * num_thread * 1000) / duration.count() << "/s\n";
+}
+
+
+int main(int argc, const char **argv) {
+  if (argc < 2) return 1;
+  auto num_thread = atoi(argv[1]);
+  parser_pure_parse_test(num_thread);
+  return 0;
+}
+
