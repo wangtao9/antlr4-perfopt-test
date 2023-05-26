d461b754bf6df01462d5e81585b0cd21d1ba08be
wangtao9 <wangtaofighting@163.com>
2023-05-26 04:58:28 +0000

    perf test on branch fix_cpp_leak_risk

--- build_all_deps.sh
+++ build_all_deps.sh
@@ -2,12 +2,12 @@
 # build wangtao9/antlr4
 TOP_LEVEL_DIR=${PWD}
 git clone https://github.com/wangtao9/antlr4.git
-cd antlr4 && git checkout -b feat_lock_free_cpp_target origin/feat_lock_free_cpp_target && mvn -DskipTests install
+cd antlr4 && git checkout -b fix_cpp_leak_risk origin/fix_cpp_leak_risk && mvn -DskipTests install
 
 
 # generate cpp lexer & parser
 cd $TOP_LEVEL_DIR
-ANTLR4_JAR=${PWD}/antlr4/tool/target/antlr4-4.12.1-SNAPSHOT-complete.jar
+ANTLR4_JAR=${PWD}/antlr4/tool/target/antlr4-4.13.1-SNAPSHOT-complete.jar
 java -jar ${ANTLR4_JAR} -Dlanguage=Cpp -visitor -o src/generated -package parser Cypher.g4
 
 
